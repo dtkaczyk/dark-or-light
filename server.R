ed9e85e52c0f7af54cced671790fb21f84f6e18b
Dominika Tkaczyk <dtkaczyk@icm.edu.pl>
2016-10-10 01:25:42 +0200

    formatting corrected

--- server.R
+++ server.R
@@ -9,13 +9,13 @@ shinyServer(function(input, output) {
     output$inputImage <- renderImage({
         image <- input$image
         if (is.null(image)) {
-            return(list(src=""))
+            return(list(src = ""))
         }
         return(list(
-                src = image$datapath,
+                src      = image$datapath,
                 filetype = "image/jpeg",
-                alt = "Original image",
-                width=250
+                alt      = "Original image",
+                width    = 250
             ))
     }, deleteFile = FALSE)
     
@@ -33,14 +33,14 @@ shinyServer(function(input, output) {
         sampleLen <- sampleLenX * sampleLenY
         sampleX <- sample(1:lenX, sampleLenX)
         sampleY <- sample(1:lenY, sampleLenY)
-        samplePixels <- pixels[sampleX,sampleY,]
+        samplePixels <- pixels[sampleX, sampleY,]
         
         data <- data.frame(R = rep(0, sampleLen), 
                            G = rep(0, sampleLen), 
                            B = rep(0, sampleLen))
         i <- 1
         for (sx in sampleX) {
-            data[((i - 1) * sampleLenY + 1):(i * sampleLenY),] <- pixels[sx,sampleY,]
+            data[((i-1)*sampleLenY+1):(i*sampleLenY),] <- pixels[sx,sampleY,]
             i <- i + 1
         }
         
@@ -52,66 +52,82 @@ shinyServer(function(input, output) {
         darkMean <- rgb(mean(dark$R), mean(dark$G), mean(dark$B))
         lightMean <- rgb(mean(light$R), mean(light$G), mean(light$B))
         
-        twoColors <- data.frame(color = c(darkMean, lightMean), 
-                             percentage = c(nrow(dark)/nrow(data), nrow(light)/nrow(data)))
+        twoColors <- data.frame(color      = c(darkMean, lightMean), 
+                                percentage = c(nrow(dark)/nrow(data), 
+                                               nrow(light)/nrow(data)))
         
-        clusteringDark <- kcca(dark[,c(1,2,3)], 9, family = kccaFamily("kmedians"))
-        darkColors <- apply(clusteringDark@centers, 1, function(x) rgb(x[1], x[2], x[3]))
+        clusteringDark <- kcca(dark[,c(1,2,3)], 9, 
+                               family = kccaFamily("kmedians"))
+        darkColors <- apply(clusteringDark@centers, 1, 
+                            function(x) rgb(x[1], x[2], x[3]))
         
-        clusteringLight <- kcca(light[,c(1,2,3)], 9, family = kccaFamily("kmedians"))
-        lightColors <- apply(clusteringLight@centers, 1, function(x) rgb(x[1], x[2], x[3]))
+        clusteringLight <- kcca(light[,c(1,2,3)], 9, 
+                                family = kccaFamily("kmedians"))
+        lightColors <- apply(clusteringLight@centers, 1, 
+                             function(x) rgb(x[1], x[2], x[3]))
         
         sortedPixels <- array(0, dim = c(sampleLenX, sampleLenY, 3))
         sortedPixels[,,1] <- matrix(data$R, nrow = sampleLenX, ncol = sampleLenY)
         sortedPixels[,,2] <- matrix(data$G, nrow = sampleLenX, ncol = sampleLenY)
         sortedPixels[,,3] <- matrix(data$B, nrow = sampleLenX, ncol = sampleLenY)
-        sortedFile <- tempfile(pattern = "file", tmpdir = tempdir(), fileext = "jpg")
+        sortedFile <- tempfile(pattern = "file", tmpdir = tempdir(), 
+                               fileext = "jpg")
         writeJPEG(sortedPixels, target = sortedFile)
          
-        list(twoColors = twoColors, darkColors = darkColors, lightColors = lightColors, sortedFile = sortedFile)
+        list(twoColors   = twoColors,
+             darkColors  = darkColors,
+             lightColors = lightColors,
+             sortedFile  = sortedFile
+        )
     })
     
     output$barplot <- renderPlot({
         twoColors <- colorSummary()$twoColors
         colorScale <- sort(as.character(twoColors$color))
         
-        ggplot(twoColors,
-               aes(x = twoColors$color, y = twoColors$percentage, fill = twoColors$color)) +
+        ggplot(twoColors, aes(x = twoColors$color, y = twoColors$percentage,
+                              fill = twoColors$color)) +
             geom_bar(stat = "identity") + 
             scale_fill_manual(values = colorScale) +
             xlab("Colors") + ylab("Fraction of the image") +
-            theme(text=element_text(size=20)) +
-            guides(fill=FALSE)
+            theme(text = element_text(size = 20)) +
+            guides(fill = FALSE)
     })
     
     output$darkColors <- renderPlot({
         cols <- colorSummary()$darkColors
         if (!is.null(cols)) {
-            gs <- lapply(cols, function(x) grobTree(rectGrob(gp=gpar(fill=x, col="white")), textGrob("")))
-            grid.arrange(grobs=gs, ncol=3, top = textGrob("Dark colors", gp=gpar(fontsize=20)))
-            grid.rect(gp=gpar(fill=NA))
+            gs <- lapply(cols, 
+                         function(x) grobTree(rectGrob(
+                             gp = gpar(fill = x, col = "white")), textGrob("")))
+            grid.arrange(grobs = gs, ncol = 3, 
+                         top = textGrob("Dark colors", gp = gpar(fontsize = 20)))
+            grid.rect(gp = gpar(fill = NA))
         }
     })
     
     output$lightColors <- renderPlot({
         cols <- colorSummary()$lightColors
         if (!is.null(cols)) {
-            gs <- lapply(cols, function(x) grobTree(rectGrob(gp=gpar(fill=x, col="white")), textGrob("")))
-            grid.arrange(grobs=gs, ncol=3, top = textGrob("Light colors", gp=gpar(fontsize=20)))
-            grid.rect(gp=gpar(fill=NA))
+            gs <- lapply(cols, 
+                         function(x) grobTree(rectGrob(
+                             gp = gpar(fill = x, col = "white")), textGrob("")))
+            grid.arrange(grobs = gs, ncol=3, 
+                         top = textGrob("Light colors", gp = gpar(fontsize = 20)))
+            grid.rect(gp = gpar(fill = NA))
         }
     })
     
     output$sortedImage <- renderImage({
         path <- colorSummary()$sortedFile
         if (is.null(path)) {
-            return(list(src=""))
+            return(list(src = ""))
         }
         return(list(
-            src = path,
+            src      = path,
             filetype = "image/jpeg",
-            alt = "Sorted image",
-            width=250
+            alt      = "Sorted image",
+            width    = 250
         ))
     }, deleteFile = FALSE)
 })
